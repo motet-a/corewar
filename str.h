ab259dbdfcdb0fcf9865e558a9c7382e6e652d52
Antoine Motet <motet_a@epitech.eu>
2016-02-22 23:35:32 +0100

    Add str.h

---
+++ str.h
@@ -0,0 +1,67 @@
+/*
+** str.h for  in /home/antoine
+**
+** Made by antoine
+** Login   <antoine@epitech.net>
+**
+** Started on  Mon Feb 22 22:59:36 2016 antoine
+** Last update Mon Feb 22 22:59:36 2016 antoine
+*/
+
+#ifndef STR_H
+# define STR_H
+
+/*
+** Like strlen().
+*/
+int             string_get_length(const char *string);
+
+/*
+** Like strcat().
+*/
+void            string_concat(char *dest, const char *source);
+
+/*
+** Like string_concat(), but appends a character.
+*/
+void            string_concat_char(char *dest, char source);
+
+void            string_copy(char *dest, const char *source);
+void            string_copy_n(char *dest, const char *source, int n);
+
+/*
+** Like strdup().
+** Can return NULL.
+*/
+char            *string_duplicate(const char *source);
+
+/*
+** Like strndup().
+** Can return NULL.
+*/
+char            *string_duplicate_n(const char *source, int n);
+
+/*
+** Like strchr().
+**
+** Returns an pointer to the first occurrence of the character c
+** in the string. Returns NULL if the given string doesn't contain c.
+*/
+const char      *string_find_char(const char *string, char c);
+
+/*
+** Like string_find_char(), but ignore the case of both arguments.
+*/
+const char      *string_find_char_no_case(const char *string, char c);
+
+/*
+** Like strstr().
+*/
+const char      *string_find(const char *string, const char *needle);
+
+/*
+** Like string_find(), but ignore the case of both arguments.
+*/
+const char      *string_find_no_case(const char *string, const char *needle);
+
+#endif /* STR_H */
