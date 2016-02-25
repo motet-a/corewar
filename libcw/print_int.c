28a4586744999ef691c8444650837ab766f1fcf1
Bailly <jamy.bailly@epitech.eu>
2016-02-25 17:29:05 +0100

    Fix norm error

--- libcw/print_int.c
+++ libcw/print_int.c
@@ -5,7 +5,7 @@
 ** Login   <bailly_j@epitech.net>
 ** 
 ** Started on  Thu Feb 25 15:19:21 2016 Jamy Bailly
-** Last update Thu Feb 25 17:01:07 2016 Jamy Bailly
+** Last update Thu Feb 25 17:28:15 2016 Jamy Bailly
 */
 
 #include <unistd.h>
@@ -17,18 +17,16 @@ void	print_uint_file(unsigned long n, int file_descriptor)
 
   if (n >= 10)
     print_uint_file(n / 10, file_descriptor);
-  c = nb % 10 + '0';
-  write(file_descriptor, &c, 1);  
+  c = n % 10 + '0';
+  write(file_descriptor, &c, 1);
 }
 
 void	print_int_file(long n, int file_descriptor)
 {
-  char	c;
-
   if (n < 0)
     {
       write(1, "-", 1);
-      nb = 0 - nb;
+      n = 0 - n;
     }
   print_uint_file((unsigned long) n, file_descriptor);
 }
