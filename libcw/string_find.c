ecbe0b76d1d4397df54fe91e4aa135dbea87cda6
baudra_a <antoine.baudrand@epitech.eu>
2016-02-25 23:23:47 +0100

    Add prototypes of string_starts_with() & cie in str.h

--- libcw/string_find.c
+++ libcw/string_find.c
@@ -5,33 +5,12 @@
 ** Login   <baudra_a@epitech.net>
 **
 ** Started on  Thu Feb 25 21:26:28 2016 Antoine Baudrand
-** Last update Thu Feb 25 22:44:13 2016 Antoine Baudrand
+** Last update Thu Feb 25 23:10:53 2016 Antoine Baudrand
 */
 
 #include <stdlib.h>
 #include "str.h"
 #include "char_type.h"
-
-int             string_starts_with(const char *string, const char *begin)
-{
-  while (*string && *string == *begin)
-    {
-      string++;
-      begin++;
-    }
-  return (!*begin);
-}
-
-int             string_starts_with_no_case(const char *string,
-                                           const char *begin)
-{
-  while (*string && char_to_lowercase(*string) == char_to_lowercase(*begin))
-    {
-      string++;
-      begin++;
-    }
-  return (!*begin);
-}
 
 const char      *string_find(const char *string, const char *needle)
 {
