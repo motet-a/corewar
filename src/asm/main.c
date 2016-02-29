6984b690248e5f42f40add2ccfde944aee5136cd
baudra_a <antoine.baudrand@epitech.eu>
2016-02-29 13:09:37 +0100

    Fix warning

--- src/asm/main.c
+++ src/asm/main.c
@@ -5,10 +5,12 @@
 ** Login   <antoine@epitech.net>
 **
 ** Started on  Fri Feb 26 18:03:12 2016 antoine
-** Last update Fri Feb 26 18:03:12 2016 antoine
+** Last update Mon Feb 29 13:09:21 2016 Antoine Baudrand
 */
 
 int             main(int argc, char **argv)
 {
+  (void) argc;
+  (void) argv;
   return (0);
 }
