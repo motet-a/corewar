a9d7de756a5cb13147786e339d37f7a8906f2e1d
Valentin Pichard (osx) <root@westery.eu>
2016-03-09 19:18:18 +0100

    delete last file

--- test.mk
+++ test.mk
@@ -5,7 +5,7 @@
 ## Login   <antoine@epitech.net>
 ##
 ## Started on  Thu Mar  3 10:39:22 2016 antoine
-## Last update Wed Mar  9 19:06:35 2016 Valentin Pichard
+## Last update Wed Mar  9 19:18:02 2016 Valentin Pichard
 ##
 
 TEST_SOURCES_NAMES	= test.c \
@@ -17,8 +17,7 @@ TEST_SOURCES_NAMES	= test.c \
 			test_string_find_char.c \
 			test_string_type.c \
 			test_string_type_convert.c \
-			test_token_list.c \
-			test_string_type_extend.c
+			test_token_list.c
 
 
 TEST_SOURCES		= $(foreach file,$(TEST_SOURCES_NAMES), \
