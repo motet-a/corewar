b9de32fb925c4a66d2e1a8a70bfdeee87a269563
baudra_a <antoine.baudrand@epitech.eu>
2016-03-09 05:54:42 +0100

    Add program struct

--- src/vm/vm.h
+++ src/vm/vm.h
@@ -5,12 +5,23 @@
 ** Login   <pichar_v@epitech.net>
 **
 ** Started on  Tue Mar  8 23:44:36 2016 Valentin Pichard
-** Last update Tue Mar  8 23:55:55 2016 Valentin Pichard
+** Last update Wed Mar  9 05:56:27 2016 Antoine Baudrand
 */
 
 #ifndef VM_H
 # define VM_H
 
+# define CYCLE_TO_DIE	1536
+# define CYCLE_DELTA	4
+# define NBR_LIVE	2048
+
 void		print_usage(const char *filename);
+
+typedef struct		s_program
+{
+	unsigned	pc;
+	unsigned	carry;
+	unsigned	*registers[REG_NUMBER];
+}			t_program
 
 #endif /* VM_H */
