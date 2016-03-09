5752e7511450fe4404d97898ed67dc352c19c3e6
Valentin Pichard (osx) <root@westery.eu>
2016-03-09 18:41:46 +0100

    patch string without data

--- src/libcw/string_type.c
+++ src/libcw/string_type.c
@@ -5,14 +5,17 @@
 ** Login   <pichar_v@epitech.net>
 **
 ** Started on  Wed Mar  9 00:51:11 2016 Valentin Pichard
-** Last update Wed Mar  9 01:35:09 2016 Valentin Pichard
+** Last update Wed Mar  9 18:38:56 2016 Valentin Pichard
 */
 
 #include "string_type.h"
 #include "char_type.h"
+#include "string.h"
 
 int     string_is_digit(const char *str)
 {
+  if (string_get_length(str) == 0)
+    return (0);
   while (*str)
     {
       if (!char_is_digit(*str++))
@@ -23,6 +26,8 @@ int     string_is_digit(const char *str)
 
 int     string_is_uppercase(const char *str)
 {
+  if (string_get_length(str) == 0)
+    return (0);
   while (*str)
     {
       if (!char_is_uppercase(*str++))
@@ -33,6 +38,8 @@ int     string_is_uppercase(const char *str)
 
 int     string_is_lowercase(const char *str)
 {
+  if (string_get_length(str) == 0)
+    return (0);
   while (*str)
     {
       if (!char_is_lowercase(*str++))
@@ -43,6 +50,8 @@ int     string_is_lowercase(const char *str)
 
 int     string_is_letter(const char *str)
 {
+  if (string_get_length(str) == 0)
+    return (0);
   while (*str)
     {
       if (!char_is_letter(*str++))
@@ -53,9 +62,11 @@ int     string_is_letter(const char *str)
 
 int     string_is_alphanumeric(const char *str)
 {
+  if (string_get_length(str) == 0)
+    return (0);
   while (*str)
     {
-      if (!char_is_lowercase(*str++))
+      if (!char_is_alphanumeric(*str++))
 	return (0);
     }
   return (1);
