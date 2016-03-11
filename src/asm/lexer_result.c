77044f88200c4e24ae8b494ae28fa2e1a562cf83
Antoine Motet <motet_a@epitech.eu>
2016-03-11 14:32:40 +0100

    Add file asm/lexer_result_2.c to fix a norm error

--- src/asm/lexer_result.c
+++ src/asm/lexer_result.c
@@ -11,6 +11,7 @@
 #include <assert.h>
 #include <stdlib.h>
 #include "lexer_private.h"
+#include "lexer_result_2.c"
 #include "../libcw/string.h"
 
 static t_lexer_result   lexer_result_create_tokens(t_token_list *tokens)
@@ -28,61 +29,5 @@ static t_lexer_result   lexer_result_create_error(t_syntax_error *error)
 
   r.tokens = NULL;
   r.error = error;
-  return (r);
-}
-
-static t_result create_error_result(const char *message,
-                                    const t_position *position)
-{
-  t_result      r;
-
-  r.error = syntax_error_new(position, message);
-  r.token = NULL;
-  return (r);
-}
-
-static t_result create_token_result_2(t_token *token)
-{
-  t_result      r;
-
-  r.error = NULL;
-  r.token = token;
-  return (r);
-}
-
-static t_result create_token_result(const t_position *begin,
-                                    t_token_type type)
-{
-  t_token       *token;
-
-  token = token_new(type, begin);
-  return (create_token_result_2(token));
-}
-
-static t_result create_string_token_result(const t_position *begin,
-                                           t_string_reader *reader,
-                                           t_token_type type)
-{
-  t_token       *token;
-  char          *string;
-  int           length;
-  int           begin_index;
-
-  assert(token_type_has_string_value(type));;
-  begin_index = (int)begin->index;
-  length = (int)reader->position.index - begin_index;
-  assert(length > 0);
-  string = string_duplicate_n(reader->file->content + begin_index, length);
-  token = token_new_string(type, begin, string);
-  free(string);
-  return (create_token_result_2(token));
-}
-
-static t_result create_null_result(void)
-{
-  t_result      r;
-
-  r.error = NULL;
-  r.token = NULL;
   return (r);
 }
---
+++ src/asm/lexer_result_2.c
@@ -0,0 +1,70 @@
+/*
+** lexer_result_2.c for  in /home/antoine
+**
+** Made by antoine
+** Login   <antoine@epitech.net>
+**
+** Started on  Fri Mar 11 14:27:48 2016 antoine
+** Last update Fri Mar 11 14:27:48 2016 antoine
+*/
+
+#include <assert.h>
+#include <stdlib.h>
+#include "lexer_private.h"
+#include "../libcw/string.h"
+
+static t_result create_error_result(const char *message,
+                                    const t_position *position)
+{
+  t_result      r;
+
+  r.error = syntax_error_new(position, message);
+  r.token = NULL;
+  return (r);
+}
+
+static t_result create_token_result_2(t_token *token)
+{
+  t_result      r;
+
+  r.error = NULL;
+  r.token = token;
+  return (r);
+}
+
+static t_result create_token_result(const t_position *begin,
+                                    t_token_type type)
+{
+  t_token       *token;
+
+  token = token_new(type, begin);
+  return (create_token_result_2(token));
+}
+
+static t_result create_string_token_result(const t_position *begin,
+                                           t_string_reader *reader,
+                                           t_token_type type)
+{
+  t_token       *token;
+  char          *string;
+  int           length;
+  int           begin_index;
+
+  assert(token_type_has_string_value(type));;
+  begin_index = (int)begin->index;
+  length = (int)reader->position.index - begin_index;
+  assert(length > 0);
+  string = string_duplicate_n(reader->file->content + begin_index, length);
+  token = token_new_string(type, begin, string);
+  free(string);
+  return (create_token_result_2(token));
+}
+
+static t_result create_null_result(void)
+{
+  t_result      r;
+
+  r.error = NULL;
+  r.token = NULL;
+  return (r);
+}
