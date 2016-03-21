642c282e92d7e725cbdc5b5f7a6869ca02eb4694
Valentin Pichard <valentin.pichard@epitech.eu>
2016-03-22 00:11:43 +0100

    add parse base function

--- src/vm/main.c
+++ src/vm/main.c
@@ -5,11 +5,33 @@
 ** Login   <antoine@epitech.net>
 **
 ** Started on  Mon Feb 29 17:34:08 2016 antoine
-** Last update Wed Mar 16 13:38:39 2016 Valentin Pichard
+** Last update Tue Mar 22 00:10:31 2016 Valentin Pichard
 */
 
 #include "../libcw/print.h"
 #include "vm.h"
+#include "options.h"
+
+static int              parse_options(int argc,
+                                      char **argv,
+                                      const t_option *options)
+{
+  int                   arg_index;
+  t_option_result       r;
+
+  arg_index = 0;
+  while (arg_index < argc)
+    {
+      r = option_get_next(argc, argv, &arg_index, options);
+      if (r.error_message != NULL)
+        {
+          print_string_err(r.error_message);
+          print_string_err("\n");
+          return (-1);
+        }
+    }
+  return (0);
+}
 
 int	main(int argc, char **argv)
 {
