ea9caa5cfdc5415557303fed09e61cfd3f06548d
Valentin Pichard <valentin.pichard@epitech.eu>
2016-03-24 22:57:39 +0100

    try add fork func

--- libcw/instr.c
+++ libcw/instr.c
@@ -5,7 +5,7 @@
 ** Login   <antoine@epitech.net>
 **
 ** Started on  Thu Mar 24 21:29:46 2016 antoine
-** Last update Thu Mar 24 21:29:46 2016 antoine
+** Last update Thu Mar 24 22:56:18 2016 Valentin Pichard
 */
 
 #include <stdlib.h>
@@ -70,9 +70,21 @@ static void     init_ld(t_instr_info *i)
   i->cycle_count = 5;
 }
 
+static void     init_fork(t_instr_info *i)
+{
+  i->name = "fork";
+  i->code = 3;
+  i->has_argument_descriptor = 1;
+  i->argument_count = 3;
+  i->argument_types[0] = ARGUMENT_TYPE_DIRECT;
+  i->argument_types[1] = ARGUMENT_TYPE_REGISTER;
+  i->cycle_count = 5;
+}
+
 void            instr_info_get_list(t_instr_info *list)
 {
   init_live(list++);
   init_ld(list++);
+  init_fork(list++);
   list->name = NULL;
 }
