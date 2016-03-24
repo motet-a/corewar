291f448f3925eb862ed2094527bb51fe9f3acaeb
Antoine Motet <motet_a@epitech.eu>
2016-03-24 21:55:13 +0100

    Print instructions when the VM starts
    This is for debugging purposes

--- libcw/instr.c
+++ libcw/instr.c
@@ -19,17 +19,19 @@ void                    instr_info_print(const t_instr_info *self)
   print_string("\ncode: ");
   print_int(self->code);
   print_string("\nhas_argument_descriptor: ");
-  print_int(self->has_argument_descriptor);
+  print_string(self->has_argument_descriptor ? "yes" : "no");
   print_string("\n");
 }
 
-void                    instr_info_print_all(const t_instr_info *list)
+void                    instr_info_print_list(const t_instr_info *list)
 {
   int                   i;
 
   i = -1;
   while (++i < instr_info_get_count(list))
     {
+      if (i)
+        print_string("\n");
       instr_info_print(list + i);
     }
 }
@@ -51,6 +53,7 @@ static void     init_live(t_instr_info *i)
 {
   i->name = "live";
   i->code = 1;
+  i->has_argument_descriptor = 1;
   i->argument_count = 1;
   i->argument_types[0] = ARGUMENT_TYPE_DIRECT;
   i->cycle_count = 5;
@@ -60,6 +63,7 @@ static void     init_ld(t_instr_info *i)
 {
   i->name = "ld";
   i->code = 2;
+  i->has_argument_descriptor = 1;
   i->argument_count = 2;
   i->argument_types[0] = ARGUMENT_TYPE_DIRECT;
   i->argument_types[1] = ARGUMENT_TYPE_REGISTER;
