a1acb767913358394d746ffc55af49c01faaac95
Valentin Pichard <valentin.pichard@epitech.eu>
2016-03-25 01:56:30 +0100

    del useless mk file

--- vm.mk
+++ vm.mk
@@ -5,16 +5,14 @@
 ## Login   <antoine@epitech.net>
 ##
 ## Started on  Thu Mar  3 10:29:22 2016 antoine
-## Last update Tue Mar 22 00:14:43 2016 Valentin Pichard
+## Last update Fri Mar 25 01:53:32 2016 Valentin Pichard
 ##
 
 VM_SOURCES_NAMES	= \
 			usage.c \
 			option_extend.c \
 			option.c \
-			vm_init_options.c \
-			vm_new.c \
-			vm_memory.c \
+			vm_initialize.c \
 			vm_free.c
 
 VM_SOURCES		= $(foreach file,$(VM_SOURCES_NAMES), \
