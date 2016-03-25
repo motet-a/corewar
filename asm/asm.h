35cd176c7555faf82de0fd3fbd321f5e175a5481
Antoine Motet <motet_a@epitech.eu>
2016-03-25 07:24:36 +0100

    Add structures in asm/asm.h

--- asm/asm.h
+++ asm/asm.h
@@ -11,4 +11,42 @@
 #ifndef ASM_H
 # define ASM_H
 
+# include "../libcw/instr.h"
+
+typedef struct          s_instr
+{
+  const t_instr_info    *info;
+}                       t_instr;
+
+typedef struct          s_instr_list
+{
+  t_instr               instr;
+  struct s_instr_list   *next;
+}                       t_instr_list;
+
+/*
+** If instr is NULL, the label points to the end of the program
+*/
+typedef struct          s_label
+{
+  char                  *name;
+  const t_instr         *instr;
+}                       t_label;
+
+typedef struct          s_label_list
+{
+  t_label               label;
+  struct s_label_list   *next;
+}                       t_label_list;
+
+typedef struct  s_program
+{
+  t_label_list  *labels;
+  t_instr_list  *intructions;
+}               t_program;
+
+void            t_program_free(t_program *program);
+void            t_program_print(const t_program *program);
+void            t_program_write(const t_program *program, int output_file);
+
 #endif /* ASM_H */
