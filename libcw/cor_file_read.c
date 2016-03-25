77b711fb81bbb568f65594e1ec15935e2e89fa68
baudra_a <antoine.baudrand@epitech.eu>
2016-03-25 16:37:06 +0100

    Fix cor_file_header_read
    Padding after name and comment are actually ends of strings ('\0')

--- libcw/cor_file_read.c
+++ libcw/cor_file_read.c
@@ -5,7 +5,7 @@
 ** Login   <antoine@epitech.net>
 **
 ** Started on  Thu Mar 24 21:11:27 2016 antoine
-** Last update ven. mars 25 15:11:14 2016 Antoine Baudrand
+** Last update ven. mars 25 16:35:12 2016 Antoine Baudrand
 */
 
 #include <unistd.h>
@@ -31,31 +31,14 @@ static char     *read_string(size_t length, int input_file)
   string = malloc(length + 1);
   if (!string)
     return (NULL);
-  if (write(input_file, string, length) <= 0)
+  if (read(input_file, string, length + 1) <= 0)
     {
       free(string);
       return (NULL);
     }
-  string[length] = '\0';
+  if (string[length] != '\0')
+    return (NULL);
   return (string);
-}
-
-static int	cor_file_header_read_prog_size(t_cor_file_header *self,
-					       int input_file)
-{
-  long		n;
-
-  if (cor_file_read_int_32(input_file, &n) || n != 0)
-    {
-      free(self->name);
-      return (-1);
-    }
-  if (cor_file_read_int_32(input_file, &n))
-    {
-      free(self->name);
-      return (-1);
-    }
-  return (0);
 }
 
 int             cor_file_header_read(t_cor_file_header *self,
@@ -67,15 +50,13 @@ int             cor_file_header_read(t_cor_file_header *self,
     return (-1);
   if (!(self->name = read_string(NAME_LENGTH, input_file)))
     return (-1);
-  if (cor_file_header_read_prog_size(self, input_file))
-    return (-1);
-  self->program_size = n;
-  if (!(self->comment = read_string(COMMENT_LENGTH, input_file)))
+  if (cor_file_read_int_32(input_file, &n))
     {
       free(self->name);
       return (-1);
     }
-  if (cor_file_read_int_32(input_file, &n) || n != 0)
+  self->program_size = n;
+  if (!(self->comment = read_string(COMMENT_LENGTH, input_file)))
     {
       free(self->name);
       return (-1);
