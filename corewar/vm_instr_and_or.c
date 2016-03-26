971de3863337b59d760c5dd8ac8276c28e7868cc
Bailly <jamy.bailly@epitech.eu>
2016-03-26 07:51:11 +0100

    [Add] vm_or()

--- corewar/vm_instr_and_or.c
+++ corewar/vm_instr_and_or.c
@@ -5,7 +5,7 @@
 ** Login   <bailly_j@epitech.net>
 **
 ** Started on  Sat Mar 26 06:56:51 2016 Jamy Bailly
-** Last update Sat Mar 26 07:46:20 2016 Jamy Bailly
+** Last update Sat Mar 26 07:50:23 2016 Jamy Bailly
 */
 
 #include "instructions.h"
@@ -22,4 +22,16 @@ void	vm_and(t_instruction *i)
   p2 = vm_instr_find_type_value(i, 1);
   p3 = 0;
   p3 = p1 & p2 & p3;
+}
+
+void	vm_or(t_instruction *i)
+{
+  int	p1;
+  int	p2;
+  int	p3;
+
+  p1 = vm_instr_find_type_value(i, 0);
+  p2 = vm_instr_find_type_value(i, 1);
+  p3 = 0;
+  p3 = p1 | p2 | p3;
 }
