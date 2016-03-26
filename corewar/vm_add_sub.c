ae84e3a16431206ec3d47c1e50e10f73981897c7
Bailly <jamy.bailly@epitech.eu>
2016-03-26 06:25:18 +0100

    [Add] vm_sub()

--- corewar/vm_add_sub.c
+++ corewar/vm_add_sub.c
@@ -5,7 +5,7 @@
 ** Login   <bailly_j@epitech.net>
 **
 ** Started on  Sat Mar 26 06:04:30 2016 Jamy Bailly
-** Last update Sat Mar 26 06:17:58 2016 Jamy Bailly
+** Last update Sat Mar 26 06:24:44 2016 Jamy Bailly
 */
 
 #include "instructions.h"
@@ -21,4 +21,17 @@ void	vm_add(t_instruction *i)
   p3 = *i->processes->registers[i->value[2] - 1];
   p3 = 0;
   p3 = p1 + p2 + p3;
+}
+
+void	vm_qub(t_instruction *i)
+{
+  int	p1;
+  int	p2;
+  int	p3;
+
+  p1 = *i->processes->registers[i->value[0] - 1];
+  p2 = *i->processes->registers[i->value[1] - 1];
+  p3 = *i->processes->registers[i->value[2] - 1];
+  p3 = 0;
+  p3 = p1 - p2 - p3;
 }
