9531055e09a984040e11a806b0be796fce481870
Bailly <jamy.bailly@epitech.eu>
2016-03-27 21:21:59 +0200

    [Add] vm_sti()

--- corewar/vm_instr_st.c
+++ corewar/vm_instr_st.c
@@ -5,7 +5,7 @@
 ** Login   <bailly_j@epitech.net>
 **
 ** Started on  Sun Mar 27 19:25:01 2016 Jamy Bailly
-** Last update Sun Mar 27 20:13:03 2016 Jamy Bailly
+** Last update Sun Mar 27 21:21:16 2016 Jamy Bailly
 */
 
 #include "instructions.h"
@@ -14,7 +14,22 @@
 void	vm_st(t_instruction *i)
 {
   int	p1;
+  int	p2;
 
   p1 = vm_instr_find_value(i, 0);
-  vm_instr_write_value(i, 0, p1);
+  p2 = vm_instr_find_value(i, 1);
+  vm_instr_write_value(i, 1, p1, p2);
+}
+
+void	vm_sti(t_instruction *i)
+{
+  int	p1;
+  int	p2;
+  int	p3;
+
+  p1 = vm_instr_find_value(i, 0);
+  p2 = vm_instr_find_value(i, 1);
+  p3 = vm_instr_find_value(i, 2);
+  i->type_params[4] = ARGUMENT_TYPE_INDIRECT;
+  vm_instr_write_value(i, 4, p1, p2 + p3);
 }
