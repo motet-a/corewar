9d32645468e29a6a45a01b4a527394fd052043e0
Bailly <jamy.bailly@epitech.eu>
2016-03-27 19:06:43 +0200

    [Add] IDX_MOD in instructions

--- corewar/vm_instr_find_address.c
+++ corewar/vm_instr_find_address.c
@@ -5,7 +5,7 @@
 ** Login   <bailly_j@epitech.net>
 **
 ** Started on  Sun Mar 27 17:31:56 2016 Jamy Bailly
-** Last update Sun Mar 27 17:51:08 2016 Jamy Bailly
+** Last update Sun Mar 27 18:46:25 2016 Jamy Bailly
 */
 
 #include "instructions.h"
@@ -15,6 +15,6 @@ int	vm_instr_find_address(t_instruction *i, int pos)
   if (i->type_params[pos] == ARGUMENT_TYPE_DIRECT)
     return (i->value[pos]);
   else if (i->type_params[pos] == ARGUMENT_TYPE_INDIRECT)
-    return (i->processes->pc + i->value[pos]);
+    return (i->processes->pc + (i->value[pos] % IDX_MOD));
   return (0);
 }
--- corewar/vm_instr_find_type_value.c
+++ corewar/vm_instr_find_type_value.c
@@ -5,7 +5,7 @@
 ** Login   <bailly_j@epitech.net>
 **
 ** Started on  Sat Mar 26 07:41:59 2016 Jamy Bailly
-** Last update Sun Mar 27 17:44:00 2016 Jamy Bailly
+** Last update Sun Mar 27 19:03:24 2016 Jamy Bailly
 */
 
 #include "instructions.h"
@@ -13,11 +13,16 @@
 
 int	vm_instr_find_value(t_instruction *i, int pos)
 {
+  int	relative;
+
   if (i->type_params[pos] == ARGUMENT_TYPE_REGISTER)
     return (i->processes->registers[i->value[pos] - 1]);
   else if (i->type_params[pos] == ARGUMENT_TYPE_DIRECT)
     return (i->value[pos]);
   else if (i->type_params[pos] == ARGUMENT_TYPE_INDIRECT)
-    return (memory_read_int_32(i->memory + i->processes->pc + i->value[pos]));
+    {
+      relative = i->processes->pc + (i->value[pos] % IDX_MOD);
+      return (memory_read_int_32(i->memory + relative));
+    }
   return (0);
 }
