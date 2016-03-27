52438e6a918a841a310e92e7b73d2b29e3cab50d
Bailly <jamy.bailly@epitech.eu>
2016-03-27 22:13:54 +0200

    [Remove] IDX_MOD in vm_instr_find_address_no_idx && vm_instr_find_value_no_idx because i'am a faggot

--- corewar/vm_instr_read_int.c
+++ corewar/vm_instr_read_int.c
@@ -5,7 +5,7 @@
 ** Login   <bailly_j@epitech.net>
 **
 ** Started on  Sat Mar 26 07:41:59 2016 Jamy Bailly
-** Last update Sun Mar 27 22:07:43 2016 Jamy Bailly
+** Last update Sun Mar 27 22:13:29 2016 Jamy Bailly
 */
 
 #include "instructions.h"
@@ -37,7 +37,7 @@ int	vm_instr_find_value_no_idx(t_instruction *i, int pos)
     return (i->value[pos]);
   else if (i->type_params[pos] == ARGUMENT_TYPE_INDIRECT)
     {
-      relative = i->processes->pc + (i->value[pos] % IDX_MOD);
+      relative = i->processes->pc + (i->value[pos]);
       return (memory_read_int_32(i->memory + relative));
     }
   return (0);
@@ -57,6 +57,6 @@ int	vm_instr_find_address_no_idx(t_instruction *i, int pos)
   if (i->type_params[pos] == ARGUMENT_TYPE_DIRECT)
     return (i->value[pos]);
   else if (i->type_params[pos] == ARGUMENT_TYPE_INDIRECT)
-    return (i->processes->pc + (i->value[pos] % IDX_MOD));
+    return (i->processes->pc + (i->value[pos]));
   return (0);
 }
