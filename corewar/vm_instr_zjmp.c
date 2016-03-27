f4c0314a4a75aa26284ba325e026401f7de7fc56
Bailly <jamy.bailly@epitech.eu>
2016-03-27 15:00:01 +0200

    [Edit] vm_zjmp() fix compatible value

--- corewar/vm_instr_zjmp.c
+++ corewar/vm_instr_zjmp.c
@@ -5,7 +5,7 @@
 ** Login   <bailly_j@epitech.net>
 **
 ** Started on  Sat Mar 26 20:00:55 2016 Jamy Bailly
-** Last update Sat Mar 26 20:22:03 2016 Jamy Bailly
+** Last update Sun Mar 27 14:59:41 2016 Jamy Bailly
 */
 
 #include "instructions.h"
@@ -13,11 +13,6 @@
 
 void	vm_zjmp(t_instruction *i)
 {
-  char	*pos;
-
   if (i->processes->carry)
-    {
-      pos = i->memory + i->processes->pc + i->value[0];
-      i->processes->pc += memory_read_int_32(pos);
-    }
+      i->processes->pc += vm_instr_find_value(i, 0);
 }
