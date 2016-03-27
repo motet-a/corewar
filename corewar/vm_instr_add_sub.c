812c764d071b6a6e45d7ec71fa2f75668513bd69
Bailly <jamy.bailly@epitech.eu>
2016-03-27 16:25:27 +0200

    [Edit] vm_instr_add_sub: add carry

--- corewar/vm_instr_add_sub.c
+++ corewar/vm_instr_add_sub.c
@@ -5,7 +5,7 @@
 ** Login   <bailly_j@epitech.net>
 **
 ** Started on  Sat Mar 26 07:40:41 2016 Jamy Bailly
-** Last update Sun Mar 27 16:00:05 2016 Jamy Bailly
+** Last update Sun Mar 27 16:19:49 2016 Jamy Bailly
 */
 
 #include "instructions.h"
@@ -14,18 +14,30 @@ void	vm_add(t_instruction *i)
 {
   int	p1;
   int	p2;
+  int	out;
 
   p1 = i->processes->registers[i->value[0] - 1];
   p2 = i->processes->registers[i->value[1] - 1];
-  i->processes->registers[i->value[2]] =  p1 + p2;
+  out = p1 + p2;
+  i->processes->registers[i->value[2]]=  out;
+  if (out == 0)
+    i->processes->carry = 1;
+  else
+    i->processes->carry = 0;
 }
 
-void	vm_qub(t_instruction *i)
+void	vm_sub(t_instruction *i)
 {
   int	p1;
   int	p2;
+  int	out;
 
   p1 = i->processes->registers[i->value[0] - 1];
   p2 = i->processes->registers[i->value[1] - 1];
+  out = p1 - p2;
   i->processes->registers[i->value[2]] = p1 - p2;
+  if (out == 0)
+    i->processes->carry = 1;
+  else
+    i->processes->carry = 0;
 }
