305d76ea79aa228370a864d94cca27b007a62b99
Bailly <jamy.bailly@epitech.eu>
2016-03-28 00:40:23 +0200

    [Add] vm_lfork()

--- corewar/vm_instr_fork.c
+++ corewar/vm_instr_fork.c
@@ -5,7 +5,7 @@
 ** Login   <bailly_j@epitech.net>
 **
 ** Started on  Mon Mar 28 00:15:13 2016 Jamy Bailly
-** Last update Mon Mar 28 00:31:06 2016 Jamy Bailly
+** Last update Mon Mar 28 00:36:49 2016 Jamy Bailly
 */
 
 #include "instructions.h"
@@ -17,5 +17,15 @@ void	vm_fork(t_instruction *i)
 
   p1 = i->value[0];
   new_pc = i->processes->pc + p1 % IDX_MOD;
+  vm_create_process(i->vm, new_pc, i->processes->program);
+}
+
+void	vm_lfork(t_instruction *i)
+{
+  int	p1;
+  int	new_pc;
+
+  p1 = i->value[0];
+  new_pc = i->processes->pc + p1;
   vm_create_process(i->vm, new_pc, i->processes->program);
 }
