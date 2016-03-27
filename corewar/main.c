4d7e723f47979b9440a8cc937590722c33e22e1c
Antoine Motet <motet_a@epitech.eu>
2016-03-27 21:30:34 +0200

    vm: Add program options

--- corewar/main.c
+++ corewar/main.c
@@ -10,40 +10,116 @@
 
 #include "../libcw/print.h"
 #include "../libcw/instr.h"
+#include "../libcw/string.h"
+#include "../libcw/parse_int.h"
 #include "vm.h"
 #include <stdio.h>
-#include "options.h"
+
+void                    program_opt_init(t_program_opt *program)
+{
+  program->name = NULL;
+  program->number = -1;
+  program->address = -1;
+}
+
+static int              set_program_number(t_program_opt *program,
+                                           const char *string)
+{
+  long                  n;
+
+  if (parse_int(string, &n) || n < 0)
+    {
+      print_string_err("Invalid program number\n");
+      return (-1);
+    }
+  program->number = n;
+  return (0);
+}
+
+static int              set_option(t_program_opt *program,
+                                   const t_option_result *r,
+                                   t_vm *vm)
+{
+  if (string_equals(r->option->name, "n"))
+    return (set_program_number(program, r->value));
+  else if (string_equals(r->option->name, "n"))
+    return (0); /* TODO:  */
+  print_string_err("Unexpected option '-");
+  print_string_err(r->option->name);
+  print_string_err("'\n");
+  return (-1);
+}
+
+static int              parse_program_options(t_parser_state *state,
+                                              t_program_opt *program,
+                                              t_vm *vm)
+{
+  t_option_result       r;
+
+  program_opt_init(program);
+  while (state->index < state->argc)
+    {
+      if (state->argv[state->index][0] != '-')
+        break;
+      r = option_get_next(state->argc, state->argv,
+                          &state->index, state->options);
+      if (r.error_message)
+        {
+          print_string_err(r.error_message);
+          print_string_err("\n");
+          return (-1);
+        }
+      if (set_option(program, &r, vm))
+        return (-1);
+    }
+  return (0);
+}
+
+static int              parse_program_opt(t_parser_state *state,
+                                          t_program_opt *program,
+                                          t_vm *vm)
+{
+  if (parse_program_options(state, program, vm))
+    return (-1);
+  if (state->index >= state->argc)
+    {
+      print_string("Expected program name\n");
+      return (-1);
+    }
+  if (state->argv[state->index][0] != '-')
+    {
+      print_string("This is a program: ");
+      print_string(state->argv[state->index]);
+      print_string("\n");
+      state->index++;
+    }
+  return (0);
+}
+
+static int              parse_programs(t_parser_state *state, t_vm *vm)
+{
+  t_program_opt         program;
+
+  while (state->index < state->argc)
+    {
+      if (parse_program_opt(state, &program, vm))
+        return (-1);
+    }
+  return (0);
+}
 
 static int              parse_options(int argc,
                                       char **argv,
                                       const t_option *options,
                                       t_vm *vm)
 {
-  int                   arg_index;
-  t_option_result       r;
+  t_parser_state        state;
 
-  arg_index = 0;
-  while (arg_index < argc)
-    {
-      if (argv[arg_index][0] != '-')
-        {
-          print_string("This is a program: ");
-          print_string(argv[arg_index]);
-          print_string("\n");
-          arg_index++;
-        }
-      else
-        {
-          r = option_get_next(argc, argv, &arg_index, options);
-          if (r.error_message != NULL)
-            {
-              print_string_err(r.error_message);
-              print_string_err("\n");
-              return (-1);
-            }
-        }
-    }
-  return (0);
+  state.argv = argv;
+  state.argc = argc;
+  state.options = options;
+  state.index = 0;
+  return (parse_programs(&state, vm));
 }
 
 int			main(int argc, char **argv)
