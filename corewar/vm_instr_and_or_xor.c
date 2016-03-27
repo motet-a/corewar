ff6883e282da77c0a95feeebce615938f3d5bdfd
Bailly <jamy.bailly@epitech.eu>
2016-03-27 16:52:28 +0200

    [Edit] vm_and(), vm_or() and vm_xor() use vm_edit_value_carry()

--- corewar/vm_instr_and_or_xor.c
+++ corewar/vm_instr_and_or_xor.c
@@ -5,7 +5,7 @@
 ** Login   <bailly_j@epitech.net>
 **
 ** Started on  Sat Mar 26 06:56:51 2016 Jamy Bailly
-** Last update Sun Mar 27 16:27:14 2016 Jamy Bailly
+** Last update Sun Mar 27 16:50:25 2016 Jamy Bailly
 */
 
 #include "instructions.h"
@@ -21,29 +21,31 @@ void	vm_and(t_instruction *i)
   p1 = vm_instr_find_value(i, 0);
   p2 = vm_instr_find_value(i, 1);
   out = p1 & p2;
-  i->processes->registers[i->value[2]] = out;
-  if (out == 0)
-    i->processes->carry = 1;
-  else
-    i->processes->carry = 0;
+  i->processes->carry = vm_edit_value_carry(out);
 }
 
 void	vm_or(t_instruction *i)
 {
   int	p1;
   int	p2;
+  int	out;
 
   p1 = vm_instr_find_value(i, 0);
   p2 = vm_instr_find_value(i, 1);
-  i->processes->registers[i->value[2]] = p1 | p2;
+  out = p1 | p2;
+  i->processes->registers[i->value[2]] = out;
+  i->processes->carry = vm_edit_value_carry(out);
 }
 
 void	vm_xor(t_instruction *i)
 {
   int	p1;
   int	p2;
+  int	out;
 
   p1 = vm_instr_find_value(i, 0);
   p2 = vm_instr_find_value(i, 1);
-  i->processes->registers[i->value[2]] = p1 ^ p2;
+  out = p1 ^ p2;
+  i->processes->registers[i->value[2]] = out;
+  i->processes->carry = vm_edit_value_carry(out);
 }
