a2198fad56e317651762732b1b5fc36f1e5b4f47
Bailly <jamy.bailly@epitech.eu>
2016-03-27 21:56:00 +0200

    [Add] vm_instr_find_value_no_idx()

--- corewar/vm_instr_read_int.c
+++ corewar/vm_instr_read_int.c
@@ -5,7 +5,7 @@
 ** Login   <bailly_j@epitech.net>
 **
 ** Started on  Sat Mar 26 07:41:59 2016 Jamy Bailly
-** Last update Sun Mar 27 20:00:50 2016 Jamy Bailly
+** Last update Sun Mar 27 21:54:38 2016 Jamy Bailly
 */
 
 #include "instructions.h"
@@ -16,7 +16,23 @@ int	vm_instr_find_value(t_instruction *i, int pos)
   int	relative;
 
   if (i->type_params[pos] == ARGUMENT_TYPE_REGISTER)
-    return (i->processes->registers[i->value[pos]]);
+    return (i->processes->registers[i->value[pos] - 1]);
+  else if (i->type_params[pos] == ARGUMENT_TYPE_DIRECT)
+    return (i->value[pos]);
+  else if (i->type_params[pos] == ARGUMENT_TYPE_INDIRECT)
+    {
+      relative = i->processes->pc + (i->value[pos] % IDX_MOD);
+      return (memory_read_int_32(i->memory + relative));
+    }
+  return (0);
+}
+
+int	vm_instr_find_value_no_idx(t_instruction *i, int pos)
+{
+  int	relative;
+
+  if (i->type_params[pos] == ARGUMENT_TYPE_REGISTER)
+    return (i->processes->registers[i->value[pos] - 1]);
   else if (i->type_params[pos] == ARGUMENT_TYPE_DIRECT)
     return (i->value[pos]);
   else if (i->type_params[pos] == ARGUMENT_TYPE_INDIRECT)
