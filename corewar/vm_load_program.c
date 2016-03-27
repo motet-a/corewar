bd25b2e5b52391bf8445e759b542fd7ce7a3a43f
Antoine Motet <motet_a@epitech.eu>
2016-03-27 19:08:53 +0200

    Simplify VM initialization

--- corewar/vm_load_program.c
+++ corewar/vm_load_program.c
@@ -1,9 +1,9 @@
 /*
 ** vm_load_program.c for corewar in /home/antoine/corewar/corewar
-** 
+**
 ** Made by Antoine Baudrand
 ** Login   <baudra_a@epitech.net>
-** 
+**
 ** Started on  ven. mars 25 18:45:49 2016 Antoine Baudrand
 ** Last update dim. mars 27 13:18:16 2016 Antoine Baudrand
 */
@@ -17,45 +17,69 @@
 #include "unistd.h"
 #include "../libcw/print.h"
 
-static void	create_program(t_vm *vm, const t_cor_file_header *header,
-			       int index)
+static int      read_program_code(t_program *program, int fd)
 {
-  vm->programs[index].name = header->name;
-  vm->programs[index].comment = header->comment;
-  vm->programs[index].last_live_cycle = 0;
-  vm->programs[index].alive = 1;
-}
-
-static void	load_in_memory(int fd, int program_size, int pc, char *memory)
-{
-  if (read(fd, &memory[pc], program_size) != program_size)
+  program->code = malloc(program->size);
+  if (!program->code)
+    return (-1);
+  if (read(fd, program->code, program->size) != program->size)
     {
       print_string_err("Error .cor file corrupted\n");
-      exit(-1);
+      free(program->code);
+      return (-1);
     }
+  return (0);
 }
 
-int				vm_load_program(t_vm *vm, const char *file, int index)
+static int      create_program(t_vm *vm,
+                               const t_cor_file_header *header,
+                               int fd)
 {
-  int				fd;
-  t_cor_file_header		header;
-  int				pc;
+  t_program     *p;
 
-  if ((fd = open(file, O_RDONLY)) >= 0)
-    {
-      if (cor_file_header_read(&header, fd))
-	return (-1);
-      pc = (MEMORY_SIZE / vm->program_count) * index;
-      create_program(vm, &header, index);
-      if (header.program_size > (MEMORY_SIZE / vm->program_count))
-	return (-1);
-      else
-	{
-	  load_in_memory(fd, header.program_size, pc, vm->memory);
-	  vm_create_process(vm, pc, &vm->programs[index]);
-	}
-      return (0);
-    }
-  else
+  vm->programs = realloc(vm->programs, vm->program_count + 1);
+  if (!vm->programs)
     return (-1);
+  p = vm->programs + vm->program_count;
+  p->name = header->name;
+  p->comment = header->comment;
+  p->last_live_cycle = 0;
+  p->alive = 1;
+  p->size = header->program_size;
+  if (read_program_code(p, fd))
+    return (-1);
+  vm->program_count++;
+  return (0);
+}
+
+static int              read_program(t_vm *vm, int fd)
+{
+  t_cor_file_header     header;
+
+  if (cor_file_header_read(&header, fd))
+    return (-1);
+  if (header.program_size > (MEMORY_SIZE / vm->program_count))
+    {
+      cor_file_header_free(&header);
+      return (-1);
+    }
+  if (create_program(vm, &header, fd))
+    {
+      cor_file_header_free(&header);
+      return (-1);
+    }
+  cor_file_header_free(&header);
+  return (0);
+}
+
+int     vm_load_program(t_vm *vm, const char *file)
+{
+  int   fd;
+  int   r;
+
+  if ((fd = open(file, O_RDONLY)) < 0)
+    return (-1);
+  r = read_program(vm, fd);
+  close(fd);
+  return (r);
 }
