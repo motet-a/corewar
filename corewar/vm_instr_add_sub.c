93ba1d5236143d08ecba8418f9293decbf89af81
Bailly <jamy.bailly@epitech.eu>
2016-03-27 16:14:18 +0200

    [Edit] vm_add() & vm_sub() because i'm a verry fagot

--- corewar/vm_instr_add_sub.c
+++ corewar/vm_instr_add_sub.c
@@ -5,7 +5,7 @@
 ** Login   <bailly_j@epitech.net>
 **
 ** Started on  Sat Mar 26 07:40:41 2016 Jamy Bailly
-** Last update sam. mars 26 12:20:48 2016 Antoine Baudrand
+** Last update Sun Mar 27 16:00:05 2016 Jamy Bailly
 */
 
 #include "instructions.h"
@@ -14,24 +14,18 @@ void	vm_add(t_instruction *i)
 {
   int	p1;
   int	p2;
-  int	p3;
 
   p1 = i->processes->registers[i->value[0] - 1];
   p2 = i->processes->registers[i->value[1] - 1];
-  p3 = i->processes->registers[i->value[2] - 1];
-  p3 = 0;
-  p3 = p1 + p2 + p3;
+  i->processes->registers[i->value[2]] =  p1 + p2;
 }
 
 void	vm_qub(t_instruction *i)
 {
   int	p1;
   int	p2;
-  int	p3;
 
   p1 = i->processes->registers[i->value[0] - 1];
   p2 = i->processes->registers[i->value[1] - 1];
-  p3 = i->processes->registers[i->value[2] - 1];
-  p3 = 0;
-  p3 = p1 - p2 - p3;
+  i->processes->registers[i->value[2]] = p1 - p2;
 }
