25ac2a51d495f82481e901e9f06f90f74aad76b1
baudra_a <antoine.baudrand@epitech.eu>
2016-03-30 15:29:05 +0200

    [edit] main.c to use new arg parser

--- corewar/main.c
+++ corewar/main.c
@@ -5,7 +5,7 @@
 ** Login   <antoine@epitech.net>
 **
 ** Started on  Mon Feb 29 17:34:08 2016 antoine
-** Last update Mon Mar 28 22:03:48 2016 Antoine
+** Last update mer. mars 30 13:52:14 2016 Antoine Baudrand
 */
 
 #include <stdlib.h>
@@ -14,13 +14,19 @@
 #include "../libcw/string.h"
 #include "../libcw/parse_int.h"
 #include "vm.h"
+#include "options.h"
 
 int			main(int argc, char **argv)
 {
   const char		*program_name;
   t_vm                  vm;
+  t_vm_options          vm_options;
 
   program_name = argv[0];
+  if (argc == 1)
+    print_usage(program_name);
+  else
+    opt_parse(&vm_options, argc - 1, argv + 1);
   vm_init(&vm);
   print_usage(program_name);
   vm_free(&vm);
